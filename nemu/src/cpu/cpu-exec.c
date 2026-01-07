8d74db4b2403237a60a3576e281f7790c64098e7
jason-hue <2113130664@qq.com>
2026-01-07 21:29:53 +0800

    before starting pa2

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -83,7 +83,7 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
-    if (nemu_state.state != NEMU_RUNNING) break;
+    if (nemu_state.state != NEMU_RUNNING) break;//将state改成stop就能实现暂停执行，本质上是打破了 CPU 的取指-执行循环。
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
