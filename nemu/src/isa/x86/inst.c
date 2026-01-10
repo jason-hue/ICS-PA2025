61a9fde8c04e8d56e0e137b5e23275cc40d841c8
tracer-ics2025 <tracer@njuics.org>
2026-01-10 19:52:58 +0800

    >  compile NEMU 231220000 郑航 Linux DESKTOP-II1FQSD 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:52:58 up 55 min,  1 user,  load average: 0.41, 0.40, 0.36

--- nemu/src/isa/x86/inst.c
+++ nemu/src/isa/x86/inst.c
@@ -276,7 +276,7 @@ again:
   INSTPAT("1110 1000", call,      J,    0, push(s->snpc);s->dnpc = s->snpc + imm);
   INSTPAT("1000 0011", gp1,       SI2E, 0, gp1());
   INSTPAT("0011 0001", xor,       G2E,  0, xor(ddest,src1));
-  INSTPAT("1100 0011", pop,       N,    0, pop(s->snpc));
+  INSTPAT("1100 0011", ret,       N,    0, pop(s->dnpc));
 
   INSTPAT("???? ????", inv,       N,    0, INV(s->pc));//通配符
   INSTPAT_END();
