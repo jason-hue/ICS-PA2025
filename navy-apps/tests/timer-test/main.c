b28e75bb725a8c9942f45b6b99f5de6becb68e0e
jason-hue <2113130664@qq.com>
2026-01-31 19:20:13 +0800

    feat:implement gettimeofday

---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,20 @@
+#include <NDL.h>
+#include <stdio.h>
+
+int main() {
+    NDL_Init(0);
+    uint32_t last_ms = NDL_GetTicks();
+    int count = 0;
+
+    printf("Timer test (NDL) start...\n");
+
+    while (1) {
+        uint32_t cur_ms = NDL_GetTicks();
+        if (cur_ms - last_ms >= 500) {
+            printf("NDL Timer: 0.5s passed (Count: %d)\n", ++count);
+            last_ms = cur_ms;
+        }
+    }
+    NDL_Quit();
+    return 0;
+}
