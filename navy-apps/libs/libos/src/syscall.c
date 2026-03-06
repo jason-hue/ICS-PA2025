20f4b7f3826cdedc1b82a7b813f5ce4c2bb43bfa
jason-hue <2113130664@qq.com>
2026-03-06 15:35:52 +0800

    适配busybox

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -4,7 +4,7 @@
 #include <assert.h>
 #include <time.h>
 #include "syscall.h"
-
+#include <errno.h>
 #include <stdio.h>
 
 // helper macros
@@ -54,6 +54,10 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   register intptr_t _gpr4 asm (GPR4) = a2;
   register intptr_t ret asm (GPRx);
   asm volatile (SYSCALL : "=r" (ret) : "r"(_gpr1), "r"(_gpr2), "r"(_gpr3), "r"(_gpr4));
+  if (ret < 0) {
+    errno = -ret;
+    return -1;
+  }
   return ret;
 }
 
@@ -101,8 +105,9 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
   return _syscall_(SYS_gettimeofday, (intptr_t)tv, (intptr_t)tz, 0);
 }
 
+extern int errno;
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
-  return _syscall_(SYS_execve, (intptr_t)fname, (intptr_t)argv, (intptr_t)envp);
+  return _syscall_(SYS_execve, fname, argv, envp);
 }
 
 // Syscalls below are not used in Nanos-lite.
